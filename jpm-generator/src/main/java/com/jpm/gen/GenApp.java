5816ded03e543f2330a54c05f2cd59a751c69c16
jayleehtt <952054767@qq.com>
2018-07-09 17:55:13 +0800

    Initial commit

---
+++ jpm-generator/src/main/java/com/jpm/gen/GenApp.java
@@ -0,0 +1,12 @@
+package com.jpm.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenApp.class, args);
+    }
+}
