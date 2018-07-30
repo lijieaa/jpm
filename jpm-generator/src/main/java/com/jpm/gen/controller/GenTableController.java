6b2c19d377b59d9205a384a1b06906c5364e36a7
jayleehtt <952054767@qq.com>
2018-07-30 12:31:15 +0800

    生成表基本信息完成

--- jpm-generator/src/main/java/com/jpm/gen/controller/GenTableController.java
+++ jpm-generator/src/main/java/com/jpm/gen/controller/GenTableController.java
@@ -1,14 +1,19 @@
 package com.jpm.gen.controller;
 
+import com.jpm.common.utils.StringUtils;
 import com.jpm.gen.entity.GenTable;
 import com.jpm.gen.service.GenTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @description: 代码生成controller
@@ -27,10 +32,32 @@ public class GenTableController {
     public List<GenTable> findTableList(GenTable genTable){
         return genTableService.findTableList(genTable);
     }
+
     @RequestMapping(value = "list")
     public String index(Model model,GenTable genTable){
         List<GenTable> tableList = genTableService.findTableList(genTable);
         model.addAttribute("tables",tableList);
         return "gen/list";
     }
+
+    /**
+     * 表基本信息
+     * @param tname
+     * @return
+     */
+    @RequestMapping(value = "basic",method = RequestMethod.GET)
+    public @ResponseBody
+    Map basic(@RequestParam(value = "tname",required = true)String tname){
+        GenTable genTable=new GenTable();
+        genTable.setName(tname);
+        List<GenTable> tableList = genTableService.findTableList(genTable);
+        GenTable genTable1 = tableList.get(0);
+        Map data=new HashMap();
+       data.put("tname",genTable1.getName());
+       data.put("comments",genTable1.getComments());
+       data.put("ename",StringUtils.toCapitalizeCamelCase(genTable.getName()));
+        return data;
+    }
+
+
 }
