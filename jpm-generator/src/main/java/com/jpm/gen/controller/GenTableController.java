2035d182c7c6ab25706f402ada573f94b9c734d4
jayleehtt <952054767@qq.com>
2018-08-02 15:07:53 +0800

    获取物理表与列

--- jpm-generator/src/main/java/com/jpm/gen/controller/GenTableController.java
+++ jpm-generator/src/main/java/com/jpm/gen/controller/GenTableController.java
@@ -5,6 +5,7 @@ import com.jpm.gen.entity.GenTable;
 import com.jpm.gen.entity.GenTableColumn;
 import com.jpm.gen.service.GenTableService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +25,58 @@ import java.util.Map;
  **/
 @Controller
 @RequestMapping(value = "gen")
+@ConfigurationProperties  // 配置文件中的前缀
 public class GenTableController {
+
+    //java类型
+    private Map<String, String> javaType = new LinkedHashMap<String, String>();
+    //查询类型
+    private Map<String, String> queryType = new LinkedHashMap<String,String>();
+
+    //显示类型
+    private Map<String, String> showType = new LinkedHashMap<String,String>();
+
+    public Map<String, String> getValidator() {
+        return validator;
+    }
+
+    public void setValidator(Map<String, String> validator) {
+        this.validator = validator;
+    }
+
+    //验证器
+    private Map<String, String> validator = new LinkedHashMap<String,String>();
+
+    public Map<String, String> getShowType() {
+        return showType;
+    }
+
+    public void setShowType(Map<String, String> showType) {
+        this.showType = showType;
+    }
+
+    public Map<String, String> getQueryType() {
+        return queryType;
+    }
+
+    public void setQueryType(Map<String, String> queryType) {
+        this.queryType = queryType;
+    }
+
+
+
+
+
+    public Map<String, String> getJavaType() {
+        return javaType;
+    }
+
+    public void setJavaType(Map<String, String> javaType) {
+        this.javaType = javaType;
+    }
+
+
+
 
     @Autowired
     GenTableService genTableService;
@@ -42,11 +95,18 @@ public class GenTableController {
      */
     @RequestMapping(value = "phy_data",method = RequestMethod.GET)
     public @ResponseBody
-    GenTable getFields(@RequestParam(value = "tname",required = true)String tname){
+    Map phy_data(@RequestParam(value = "tname",required = true)String tname){
         GenTable genTable=new GenTable();
         genTable.setName(tname);
         GenTable tableFormDb = genTableService.getTableFormDb(genTable);
-        return tableFormDb;
+
+        Map data=new HashMap();
+        data.put("table",tableFormDb);
+        data.put("javaType",javaType);
+        data.put("queryType",queryType);
+        data.put("showType",showType);
+        data.put("validator",validator);
+        return data;
     }
 
 
